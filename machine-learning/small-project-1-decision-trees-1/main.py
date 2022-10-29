2b5ddaf8f1aa5c2c7501aed3136f3387d4c3d14a
Hououin Kyouma <inwazzin@gmail.com>
2022-10-29 21:54:22 +0200

    renames

--- machine-learning/small-project-1-decision-trees-1/main.py
+++ machine-learning/small-project-1-decision-trees-1/main.py
@@ -42,33 +42,34 @@ def treeify(data: dict):
       tree.create_node(str(value), node, parent=parent)
   return tree
 
+def verify(dataset: ds.Dataset, decision: str):
+  entropy = dataset.entropy(decision)
+  print(dataset)
+  print()
+
+  print(f"H(Play) Entropy of the weka weather dataset: {entropy:.2f}")
+  for class_ in dataset.classes('outlook'):
+    class_entropy = dataset[dataset['outlook'] == class_].entropy(decision)
+    print(f"{f'H(Play|Outlook={class_})': <{offset}}: {class_entropy:.2f}")
+  print()
+
+  for label in dataset.labels([decision]):
+    information = dataset.information(label, decision)
+    information_gain = dataset.information_gain(label, decision)
+    split_information = dataset.split_information(label)
+    information_gain_ratio = dataset.information_gain_ratio(label, decision)
+    print(f"{f'I({label})': <{offset}}: {information:.4f}")
+    print(f"{f'IG({label})': <{offset}}: {information_gain:.4f}")
+    print(f"{f'SI({label})': <{offset}}: {split_information:.4f}")
+    print(f"{f'IGR({label})': <{offset}}: {information_gain_ratio:.4f}")
+  print()
+
+
 offset = 24
 decision = 'survived'
 if __name__ == '__main__':
-  dataset = ds.Titanic.fromfile("resources/titanic-dataset.csv", simplify=True)
-  # entropy = dataset.entropy(decision)
-  # print(dataset)
-  # print()
-  #
-  # print(f"H(Play) Entropy of the weka weather dataset: {entropy:.2f}")
-  # for class_ in dataset.classes('outlook'):
-  #   class_entropy = dataset[dataset['outlook'] == class_].entropy(decision)
-  #   print(f"{f'H(Play|Outlook={class_})': <{offset}}: {class_entropy:.2f}")
-  # print()
-  #
-  # for label in dataset.labels([decision]):
-  #   information = dataset.information(label, decision)
-  #   information_gain = dataset.information_gain(label, decision)
-  #   split_information = dataset.split_information(label)
-  #   information_gain_ratio = dataset.information_gain_ratio(label, decision)
-  #   print(f"{f'I({label})': <{offset}}: {information:.4f}")
-  #   print(f"{f'IG({label})': <{offset}}: {information_gain:.4f}")
-  #   print(f"{f'SI({label})': <{offset}}: {split_information:.4f}")
-  #   print(f"{f'IGR({label})': <{offset}}: {information_gain_ratio:.4f}")
-  # print()
+  verify(ds.WekaWeather.fromfile("resources/weka-weather.csv"), 'play')
 
-  label = find_most_informative_feature(dataset, decision)
-  print(f"Max information gain from: {label}")
+  dataset = ds.Titanic.fromfile("resources/titanic.csv", simplify=True)
   tree = create_tree(dataset, decision)
   tree = treeify(tree)
-  tree.show()
